b4e456797a7cb3b0b0cd5cd18753394629c36ee2
int-x-survey <nakaguchi07@gmail.com>
2024-11-15 16:40:38 +0900

    復元ポイント

--- solution/src/app.tsx
+++ solution/src/app.tsx
@@ -175,7 +175,7 @@ const App = () => {
         mapId={import.meta.env.VITE_REACT_APP_GOOGLE_MAPS_MAP_ID}
       >
         <PoiMarkers pois={poisRyotsuAikawa} pinColor="#FBBC04" />
-        <PoiMarkers pois={poisKanaiSawata} pinColor="#FF0000" />
+        <PoiMarkers pois={poisKanaiSawata} pinColor="" />
         <PoiMarkers pois={poisAkadomariHamochi} pinColor='#FFFFFF' />
         </Map>
     </APIProvider>
