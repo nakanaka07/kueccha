0c57dd6d97f01d694adaef2523ab16ae5713f154
int-x-survey <nakaguchi07@gmail.com>
2024-11-21 15:45:41 +0900

    復元ポイント

--- solution/src/Map.tsx
+++ solution/src/Map.tsx
@@ -4,77 +4,82 @@ import { MarkerClusterer } from "@react-google-maps/api";
 import type { Poi } from "./types.d.ts";
 import { MAP_CONFIG } from "./appConstants";
 import InfoWindowContentMemo from "./InfoWindowContent";
-import { nanoid } from 'nanoid';
-
+import { nanoid } from "nanoid";
 
 interface MapProps {
-  pois: Poi[];
-  mapInitialized: boolean;
-  setMapInitialized: React.Dispatch<React.SetStateAction<boolean>>;
+	pois: Poi[];
+	mapInitialized: boolean;
+	setMapInitialized: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const Map = memo(
-  ({ pois, setMapInitialized }: MapProps) => {
-    console.log("Map rendered", pois);
-    const [map, setMap] = useState<google.maps.Map | null>(null);
-    const [activeMarker, setActiveMarker] = useState<Poi | null>(null);
+	({ pois, setMapInitialized }: MapProps) => {
+		console.log("Map rendered", pois);
+		const [map, setMap] = useState<google.maps.Map | null>(null);
+		const [activeMarker, setActiveMarker] = useState<Poi | null>(null);
 
-    const handleMarkerClick = useCallback((poi: Poi) => {
-      console.log("handleMarkerClick called", poi);
-      setActiveMarker(poi);
-    }, []);
+		const handleMarkerClick = useCallback((poi: Poi) => {
+			console.log("handleMarkerClick called", poi);
+			setActiveMarker(poi);
+		}, []);
 
-    const handleMapLoad = useCallback((map: google.maps.Map) => {
-      console.log("handleMapLoad called", map);
-      setMap(map);
-      setMapInitialized(true);
-    }, [setMapInitialized]);
+		const handleMapLoad = useCallback(
+			(map: google.maps.Map) => {
+				console.log("handleMapLoad called", map);
+				setMap(map);
+				setMapInitialized(true);
+			},
+			[setMapInitialized]
+		);
 
-    return (
-      <GoogleMap
-        onLoad={handleMapLoad}
-        mapContainerStyle={MAP_CONFIG.mapContainerStyle}
-        center={MAP_CONFIG.defaultCenter}
-        zoom={MAP_CONFIG.defaultZoom}
-        options={{
-          mapId: import.meta.env.VITE_REACT_APP_GOOGLE_MAPS_MAP_ID,
-          disableDefaultUI: true,
-          zoomControl: true,
-        }}
-      >
-        <MarkerClusterer>
-        {(clusterer) => (
-    <>
-      {pois.map((poi) => (
-              <Marker
-                key={nanoid()}
-                position={{ lat: poi.location.lat, lng: poi.location.lng }}
-                title={poi.name}
-                onClick={() => handleMarkerClick(poi)}
-                clusterer={clusterer}
-                />
-              ))}
-              </>
-            )}
-                  </MarkerClusterer>
+		return (
+			<GoogleMap
+				onLoad={handleMapLoad}
+				mapContainerStyle={MAP_CONFIG.mapContainerStyle}
+				center={MAP_CONFIG.defaultCenter}
+				zoom={MAP_CONFIG.defaultZoom}
+				options={{
+					mapId: import.meta.env.VITE_REACT_APP_GOOGLE_MAPS_MAP_ID,
+					disableDefaultUI: true,
+					zoomControl: true,
+				}}
+			>
+				<MarkerClusterer>
+					{(clusterer) => (
+						<>
+							{pois.map((poi) => (
+								<Marker
+									key={nanoid()}
+									position={{ lat: poi.location.lat, lng: poi.location.lng }}
+									title={poi.name}
+									onClick={() => handleMarkerClick(poi)}
+									clusterer={clusterer}
+								/>
+							))}
+						</>
+					)}
+				</MarkerClusterer>
 
-        {activeMarker && (
-          <InfoWindow
-            position={{
-              lat: activeMarker.location.lat,
-              lng: activeMarker.location.lng,
-            }}
-            onCloseClick={() => setActiveMarker(null)}
-          >
-            <InfoWindowContentMemo poi={activeMarker} />
-          </InfoWindow>
-        )}
-      </GoogleMap>
-    );
-  },
-  (prevProps: MapProps, nextProps: MapProps) => {
-    return prevProps.pois === nextProps.pois && prevProps.mapInitialized === nextProps.mapInitialized;
-  }
+				{activeMarker && (
+					<InfoWindow
+						position={{
+							lat: activeMarker.location.lat,
+							lng: activeMarker.location.lng,
+						}}
+						onCloseClick={() => setActiveMarker(null)}
+					>
+						<InfoWindowContentMemo poi={activeMarker} />
+					</InfoWindow>
+				)}
+			</GoogleMap>
+		);
+	},
+	(prevProps: MapProps, nextProps: MapProps) => {
+		return (
+			prevProps.pois === nextProps.pois &&
+			prevProps.mapInitialized === nextProps.mapInitialized
+		);
+	}
 );
 
 export default Map;
