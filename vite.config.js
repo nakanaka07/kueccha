b13c4da31843acbebbbcf5fc24078a400fbf304a
int-x-survey <nakaguchi07@gmail.com>
2025-01-03 10:33:19 +0900

    0103-9

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,15 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import replace from '@rollup/plugin-replace';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    base: mode === 'production' ? '/kueccha/' : '/', // 環境に応じてベースパスを設定
+    base: mode === 'production' ? '/kueccha/' : '/', // リポジトリ名をベースパスとして設定
     plugins: [
       react(),
       tsconfigPaths(),
-      replace({
-        'process.env.BASE_URL': JSON.stringify(mode === 'production' ? '/kueccha/' : '/'),
-        preventAssignment: true,
-      }),
     ],
     resolve: {
       alias: {
@@ -24,24 +19,6 @@ export default defineConfig(({ mode }) => {
     build: {
       outDir: 'dist',
       sourcemap: mode === 'development',
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            vendor: ['react', 'react-dom'],
-            maps: [
-              '@react-google-maps/api',
-              '@googlemaps/js-api-loader',
-              '@react-google-maps/marker-clusterer',
-              '@react-google-maps/infobox',
-              '@googlemaps/markerclusterer',
-            ],
-          },
-          assetFileNames: 'assets/[name]-[hash][extname]',
-          chunkFileNames: 'js/[name]-[hash].js',
-          entryFileNames: 'js/[name]-[hash].js',
-        },
-      },
-      target: 'es2022',
     },
     optimizeDeps: {
       include: [
@@ -56,7 +33,6 @@ export default defineConfig(({ mode }) => {
         sourcemap: false,
       },
     },
-  
     define: {
       'process.env.VITE_GOOGLE_MAPS_API_KEY': JSON.stringify(env.VITE_GOOGLE_MAPS_API_KEY),
       'process.env.VITE_GOOGLE_MAPS_MAP_ID': JSON.stringify(env.VITE_GOOGLE_MAPS_MAP_ID),
